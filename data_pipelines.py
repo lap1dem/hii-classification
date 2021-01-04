fffff6cae006555baa972ce432bc9bb0e2a273a4
lap1dem <vadym.bidula@gmail.com>
2021-01-04 13:11:59 +0200

    dataset review

--- data_pipelines.py
+++ data_pipelines.py
@@ -15,21 +15,22 @@ def load_catalogue():
     hii_data['class'] = 'HII'
 
     # SDSS classified objects
-    sdss_data = pd.read_csv('data/SDSS/sdss-all-objects.csv', skiprows=1).set_index('objid')
-    sdss_data = sdss_data[['u', 'g', 'r', 'i', 'z', 'redshift', 'ra', 'dec', 'class']]
-    sdss_data = sdss_data[sdss_data['redshift'] > 0]
+    sdss_data = pd.read_csv('data/SDSS/sdss-all-objects.csv')
+    sdss_data.rename(columns = {'bestObjID': 'objid'}, inplace=True)
+    sdss_data.set_index('objid', inplace=True)
     sdss_data = sdss_data[
+        (sdss_data['zWarning'] == 0) &
+        (sdss_data['redshift'] > 0) &
         (sdss_data['u'] > 0) &
         (sdss_data['g'] > 0) &
         (sdss_data['r'] > 0) &
         (sdss_data['i'] > 0) &
         (sdss_data['z'] > 0)
-        ]
+    ]
+    sdss_data = sdss_data[['u', 'g', 'r', 'i', 'z', 'redshift', 'ra', 'dec', 'class']]
 
     # Similar objects
-    sdss_data = sdss_data.drop(sdss_data.index.intersection(hii_data.index, sort=False))
-
-
+    sdss_data.drop(sdss_data.index[sdss_data.index.isin(hii_data.index)], inplace=True)
     return pd.concat([hii_data, sdss_data])
 
 
@@ -45,4 +46,3 @@ def gal_tt_split(frac = 0.8, random_state = 172):
 
     return X_train, y_train, X_test, y_test
 
-gal_tt_split()
--- train_model.py
+++ train_model.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 from tensorflow import keras
 import pickle
+import numpy as np
+import datetime
 
 import config as c
 from models import get_compiled_model
@@ -16,11 +18,23 @@ pickle.dump(X_test, open("xtest.p", "wb"))
 pickle.dump(y_test, open("ytest.p", "wb"))
 pickle.dump(cat, open("categories.p", "wb"))
 
-train_dataset = tf.data.Dataset.from_tensor_slices((X_train.values, y_train.values)).batch(64)
+train_dataset = tf.data.Dataset.from_tensor_slices((X_train.values, y_train.values)).batch(512)
 
 cp_callback = keras.callbacks.ModelCheckpoint(filepath=c.checkpoint_path,
                                               save_weights_only=True,
                                               verbose=1)
 
 model = get_compiled_model()
-model.fit(train_dataset, epochs=50, callbacks=[cp_callback])
+
+# ----------------
+from sklearn.utils import class_weight
+class_weights = class_weight.compute_class_weight(class_weight='balanced', classes=np.unique(y_train), y=y_train)
+class_weights_dict = dict(enumerate(class_weights))
+print(class_weights_dict)
+# ----------------
+
+log_dir = "logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+tensorboard_callback = keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
+
+
+model.fit(train_dataset, epochs=50, callbacks=[cp_callback, tensorboard_callback], class_weight=class_weights_dict)
